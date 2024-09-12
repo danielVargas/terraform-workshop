47c019ecf8a132a9c9a1961249a3324b61a8e1e9
Daniel Vargas <daniel.vargas@usach.cl>
2024-09-12 18:15:17 -0300

    feat: practica 2

--- practica_2/local_files.tf
+++ practica_2/local_files.tf
@@ -1,7 +1,7 @@
 resource "local_file" "productos" {
   count    = 3
   content  = "Lista de productos para el mes proximo"
-  filename = "productos-${random_string.sufijo[count.index].id}.txt"
+  filename = "tmp/productos-${random_string.sufijo[count.index].id}.txt" // utiliza el sufijo generado en random.tf
 }
 
 
--- practica_2/random.tf
+++ practica_2/random.tf
@@ -1,8 +1,8 @@
 resource "random_string" "sufijo" {
   count   = 3
   length  = 4
-  special = false
-  upper   = false
-  numeric = false
+  special = false // Sin caracteres especiales
+  upper   = false // Sin mayúsculas
+  numeric = false // Sin números
 }
 
