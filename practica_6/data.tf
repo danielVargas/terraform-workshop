4e56cf0394fbcbcd61e01b5a36630b36edc47eef
Daniel Vargas <daniel.vargas@usach.cl>
2024-10-03 11:17:09 -0300

    feature: datos

--- practica_6/data.tf
+++ practica_6/data.tf
@@ -1,3 +1,3 @@
-//data "aws_key_pair" "key" {
-//  key_name = "mykey"
-//}
+data "aws_key_pair" "key" {
+  key_name = "test_key_pair" // Debe coincidir con una llave pem en la infraestructura
+}
--- practica_6/ec2.tf
+++ practica_6/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "public_instance" {
   ami                    = var.ec2_specs.ami
   instance_type          = var.ec2_specs.instance_type
   subnet_id              = aws_subnet.public_subnet.id // viene del archivo de vpc.tf
-  ##key_name               = data.aws_key_pair.key.key_name
+  key_name               = data.aws_key_pair.key.key_name
   vpc_security_group_ids = [aws_security_group.sg_public_instance.id]
   user_data              = file("scripts/userdata.sh")
 
